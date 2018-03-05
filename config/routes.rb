dbcf55714de1b0127323294b7e5ce6b7cd524e09
Austin <AustinPierce@gmail.com>
2018-03-04 22:00:33 -0500

    Index to show all, change homepage to article index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index' 
   # https://ruby-on-rails-austinpierce956002.codeanyapp.com/welcome/index
 
-  root 'welcome#index'
+ # root 'welcome#index'
+  
+ root 'articles#index'
   
   resources :articles
   
