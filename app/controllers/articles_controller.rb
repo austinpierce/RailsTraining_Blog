849aeb984173dd8a60c38f832e87aac384fbb49a
Austin <AustinPierce@gmail.com>
2018-03-04 21:44:13 -0500

    Article model, create method, show view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,20 @@ class ArticlesController < ApplicationController
   def new
   end
   
+  def create
+    @article = Article.new(article_params) 
+    # Article is referring to the class in models/article.rb
+    @article.save
+    redirect_to @article
+  end
+  
+  def show
+    @article = Article.find(params[:id]) # @article is an instance variable
+  end
+  
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+  
 end
